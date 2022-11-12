5ffa10e377cefedbb6cffb89bd9fb2b795d687d1
Giovani Candido <giovcandido@outlook.com>
2022-11-12 18:00:17 -0300

    Create accounts app

--- metaopt/urls.py
+++ metaopt/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('', include('dashboard.urls')),
 ]
