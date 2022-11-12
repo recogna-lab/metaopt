39dfb085862479a65f852466e8865a6bca258dfe
Giovani Candido <giovcandido@outlook.com>
2022-11-12 18:01:20 -0300

    Redirect from index to login page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required(login_url='accounts:login', redirect_field_name='next')
 def index(request):
     return render(request, 'dashboard/index.html')
