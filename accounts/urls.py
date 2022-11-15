39858694a6ac524e6cafd52a06231317e31d824e
Giovani Candido <giovcandido@outlook.com>
2022-11-15 10:02:41 -0300

    Update login logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('login/perform/', views.perform_login, name='perform_login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from .forms import LoginForm
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def login_view(request):
+    loginForm = LoginForm()
+    
+    return render(request, 'accounts/login.html', context={
+        'form': loginForm,
+        'form_action': reverse('accounts:perform_login')
+    })
+
+def perform_login(request):
+    if not request.POST:
+        return redirect('accounts:login')
+    
+    loginForm = LoginForm(request.POST)
+    
+    if loginForm.is_valid():
+        authenticated_user = authenticate(
+            username=loginForm.cleaned_data.get('username', ''),
+            password=loginForm.cleaned_data.get('password', '')
+        )
+        
+        if authenticated_user is not None:
+            login(request, authenticated_user)
+            return redirect('dashboard:index')
+        
+        messages.error(request, 'Credenciais inválidas.')
+    else:
+        messages.error(request, 'Erro de validação.')
+    
+    return redirect('accounts:login')
