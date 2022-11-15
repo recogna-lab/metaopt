8dfa83ceecdac283a59a59c5e0533ca9eb171173
Giovani Candido <giovcandido@outlook.com>
2022-11-15 12:37:23 -0300

    Add global styles and add static dirs to settings

--- metaopt/settings.py
+++ metaopt/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
-    'widget_tweaks',
     'dashboard',
     'accounts',
 ]
@@ -122,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
