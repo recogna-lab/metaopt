dc7d811df851babbffdce54715bb06245a3c00ab
Giovani Candido <giovcandido@outlook.com>
2022-11-15 14:37:43 -0300

    Add some classes to the form template

--- accounts/forms/login.py
+++ accounts/forms/login.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from utils.django_forms import add_placeholder
+from utils.django_forms import add_attr, add_placeholder
 
 
 class LoginForm(forms.Form):
@@ -10,6 +10,9 @@ class LoginForm(forms.Form):
         add_placeholder(self.fields['username'], 'Digite seu usuário')
         add_placeholder(self.fields['password'], 'Digite sua senha')
         
+        add_attr(self.fields['username'], 'class', 'form-control')
+        add_attr(self.fields['password'], 'class', 'form-control')
+
     username = forms.CharField(label='Usuário')
     
     password = forms.CharField(
