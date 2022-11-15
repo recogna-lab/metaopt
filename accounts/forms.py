3f9541e27d9df167b3b4823dabfe61ec3b250fd7
Giovani Candido <giovcandido@outlook.com>
2022-11-15 11:47:48 -0300

    Add labels to login form and improve template

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(label='Usuário')
+    password = forms.CharField(label='Senha', widget=forms.PasswordInput())
