fd2e036ff4df5f9ed70110b6c268a6f0b07fa5f6
Giovani Candido <giovcandido@outlook.com>
2022-11-15 14:49:46 -0300

    Upload signup form

--- accounts/forms/__init__.py
+++ accounts/forms/__init__.py
@@ -1,1 +1,2 @@
 from .login import LoginForm
+from .signup import SignupForm
---
+++ accounts/forms/signup.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from utils.django_forms import add_attr, add_placeholder
+
+
+class SignupForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        add_placeholder(self.fields['first_name'], 'Digite seu nome')
+        add_placeholder(self.fields['last_name'], 'Digite seu sobrenome')
+        add_placeholder(self.fields['username'], 'Digite seu usuário')
+        add_placeholder(self.fields['email'], 'Digite seu e-mail')
+        add_placeholder(self.fields['password'], 'Digite sua senha')
+        add_placeholder(self.fields['confirm_password'], 'Confirme sua senha')
+        
+        add_attr(self.fields['first_name'], 'class', 'form-control')
+        add_attr(self.fields['last_name'], 'class', 'form-control')
+        add_attr(self.fields['username'], 'class', 'form-control')
+        add_attr(self.fields['email'], 'class', 'form-control')
+        add_attr(self.fields['password'], 'class', 'form-control')
+        add_attr(self.fields['confirm_password'], 'class', 'form-control')
+
+    first_name = forms.CharField(label='Nome')
+    last_name = forms.CharField(label='Sobrenome')
+    username = forms.CharField(label='Usuário')
+    email = forms.CharField(label='E-mail')
+    
+    password = forms.CharField(
+        label='Senha', 
+        widget=forms.PasswordInput()
+    )
+    
+    confirm_password = forms.CharField(
+        label='Confirmar senha', 
+        widget=forms.PasswordInput()
+    )
+    
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password'
+        ]
