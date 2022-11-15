2b4abc5b5a23f11b21affcc73fed6fde1761f577
Giovani Candido <giovcandido@outlook.com>
2022-11-15 15:16:34 -0300

    Add signup logic to accounts view

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,47 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 
 
 def signup(request):
-    ...
+    signup_form_data = request.session.get('signup_form_data', None)
+    
+    signup_form = SignupForm(signup_form_data)
+
+    return render(request, 'accounts/pages/signup.html', context={
+        'form': signup_form,
+        'form_action': reverse('accounts:perform_signup')
+    })
 
 def perform_signup(request):
-    ...
+    signup_url = reverse('accounts:signup')
+
+    if not request.POST:
+        return redirect(signup_url)
+
+    signup_form_data = request.POST
+    request.session['signup_form_data'] = signup_form_data
+    
+    signup_form = SignupForm(signup_form_data)
+    
+    if signup_form.is_valid():
+        user = signup_form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        
+        message = 'Usuário criado com sucesso. '
+        message += 'Por favor, logue.'
+        
+        messages.success(request, message)        
+      
+        del request.session['signup_form_data']
+
+        return redirect(reverse('accounts:login'))
+
+    messages.success(request, 'Erro de validação.')
+            
+    return redirect(signup_url)
 
 def login_view(request):
     dashboard_url = reverse('dashboard:index')
@@ -21,10 +54,10 @@ def login_view(request):
     next = request.GET.get('next', '')
     request.session['next'] = next or dashboard_url
     
-    loginForm = LoginForm()
+    login_form = LoginForm()
 
     return render(request, 'accounts/pages/login.html', context={
-        'form': loginForm,
+        'form': login_form,
         'form_action': reverse('accounts:perform_login')
     })
 
@@ -34,12 +67,12 @@ def perform_login(request):
     if not request.POST:
         return redirect(login_url)
     
-    loginForm = LoginForm(request.POST)
+    login_form = LoginForm(request.POST)
     
-    if loginForm.is_valid():
+    if login_form.is_valid():
         authenticated_user = authenticate(
-            username=loginForm.cleaned_data.get('username', ''),
-            password=loginForm.cleaned_data.get('password', '')
+            username=login_form.cleaned_data.get('username', ''),
+            password=login_form.cleaned_data.get('password', '')
         )
         
         if authenticated_user is not None:
