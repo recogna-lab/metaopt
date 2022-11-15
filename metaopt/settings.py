4085041b859987f2267c2756dd294ff3f95282ab
Giovani Candido <giovcandido@outlook.com>
2022-11-15 11:28:52 -0300

    Create base.html template

--- metaopt/settings.py
+++ metaopt/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'metaopt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
