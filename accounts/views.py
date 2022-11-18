c8406603d68d6ad3020adc4d0dcd1cf87cc88359
Luiz Sementille <luigifernando2011@gmail.com>
2022-11-18 19:05:32 -0300

    Creating reset password views

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,49 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import LoginForm, SignupForm
+from .forms import LoginForm, SignupForm, ResetPasswordForm
 
+
+def reset_password(request):
+    forgot_password_url = reverse('accounts:perform_reset_password')
+
+    forgot_form = ResetPasswordForm()
+
+    return render(request, 'accounts/pages/reset_password.html', context={
+        'form': forgot_form,
+        'form_action': forgot_password_url,
+    })
+
+def perform_reset_password(request):
+    login_url = reverse('accounts:login')
+
+    if not request.POST:
+        return redirect(reverse('accounts:reset_password'))
+
+    password_data = request.POST
+
+    request.session['password_data'] = password_data
+
+    forgot_password_form = ResetPasswordForm(password_data)
+
+    if forgot_password_form.is_valid():
+
+        if forgot_password_form.email_exists():
+
+            message = "E-mail para redefinição enviado. Por favor, verifique."
+            messages.success(request, message)   
+
+            forgot_password_form.send_email(password_data['email'])
+
+            del request.session['password_data'] 
+
+            return redirect(login_url)
+        
+        messages.error(request, 'E-mail não cadastrado.')
+    
+    messages.error(request, 'Endereço de e-mail inválido.')
+        
+    return redirect('accounts:reset_password')
 
 def signup(request):
     if request.user.is_authenticated:
@@ -26,6 +67,9 @@ def perform_signup(request):
         return redirect(signup_url)
 
     signup_form_data = request.POST
+
+    print(signup_form_data)
+
     request.session['signup_form_data'] = signup_form_data
     
     signup_form = SignupForm(signup_form_data)
@@ -70,8 +114,8 @@ def perform_login(request):
     
     if login_form.is_valid():
         authenticated_user = authenticate(
-            username=login_form.cleaned_data.get('username', ''),
-            password=login_form.cleaned_data.get('password', '')
+            username = login_form.cleaned_data.get('username', ''),
+            password = login_form.cleaned_data.get('password', '')
         )
         
         if authenticated_user is not None:
