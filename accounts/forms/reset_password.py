d30656a84aa6a2caa2cbda137a2929013ad349fd
Luiz Sementille <luigifernando2011@gmail.com>
2022-12-02 12:29:27 -0300

    Updating send email method.

--- accounts/forms/reset_password.py
+++ accounts/forms/reset_password.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib import messages
+from metaopt.settings.mail import EMAIL_HOST_PASSWORD, EMAIL_HOST_USER
 
 from utils.django_forms import add_attr, add_placeholder
 
@@ -30,10 +38,25 @@ class ResetPasswordForm(forms.Form):
         return found
 
     def send_email(self, email):
-        
-        print(f'email: {email}')
 
         user = User.objects.filter(email=email).first()
 
-        print(user.first_name)
+        subject = "Pedido de Redefinição de Senha"
+        email_template = "accounts/pages/password_reset.html"
+        c = {
+            "email" : user.email,
+            'domain' : '127.0.0.1:8000',
+            'site_name': "MetaOPT",
+            "uid" : urlsafe_base64_encode(force_bytes(user.pk)),
+            "user" : user.first_name,
+            "token" : default_token_generator.make_token(user),
+            'protocol' : 'http',
+        }
+
+        email = render_to_string(email_template, c)
+        try:
+            send_mail(subject=subject, message=email, from_email="", recipient_list=[user.email], auth_user=EMAIL_HOST_USER,
+            auth_password=EMAIL_HOST_PASSWORD, fail_silently=False)
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
 
