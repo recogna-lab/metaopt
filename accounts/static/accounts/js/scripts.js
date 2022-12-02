626dc769fc25fde19e4a8c3412f846d8dc565650
Giovani Candido <giovcandido@outlook.com>
2022-12-02 19:06:21 -0300

    Create scripts file in accounts app

---
+++ accounts/static/accounts/js/scripts.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({placement: 'bottom'})
+})
+
+const showPassword = function() {
+    const fields = []
+
+    fields[0] = document.getElementsByName('password')[0]
+    fields[1] = document.getElementsByName('confirm_password')[0]
+
+    fields.forEach((field) => {
+        if(field) {
+            field.type = field.type == 'password' ? 'text' : 'password'
+        }
+    })
+}
