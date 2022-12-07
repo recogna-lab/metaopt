556df628a9a88fb371d60bc1537e7dcf273b4268
Giovani Candido <giovcandido@outlook.com>
2022-12-07 18:35:25 -0300

    Remove unsed model

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
