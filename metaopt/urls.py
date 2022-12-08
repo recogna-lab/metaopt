12281fa6deeab27e4e05d3112fc68f485a0b0dab
Giovani Candido <giovcandido@outlook.com>
2022-12-08 14:14:35 -0300

    Reorder entries

--- metaopt/urls.py
+++ metaopt/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('', include('dashboard.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
 ]
