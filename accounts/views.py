d4c4feb1d6768f79994c2ed229b238bbea960662
Giovani Candido <giovcandido@outlook.com>
2022-12-08 09:43:30 -0300

    Reorder views file

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,94 @@ from django.urls import reverse
 from .forms import LoginForm, NewPasswordForm, ResetPasswordForm, SignupForm
 
 
+def login_view(request):
+    dashboard_url = reverse('dashboard:index')
+    
+    if request.user.is_authenticated:
+        return redirect(dashboard_url)
+    
+    next = request.GET.get('next', '')
+    request.session['next'] = next or dashboard_url
+    
+    login_form = LoginForm()
+        
+    return render(request, 'accounts/pages/login.html', context={
+        'form': login_form,
+        'form_action': reverse('accounts:perform_login'),
+        'has_password_fields': True
+    })
+
+def perform_login(request):
+    login_url = reverse('accounts:login')
+    
+    if not request.POST:
+        return redirect(login_url)
+
+    login_form = LoginForm(request.POST)
+    
+    if login_form.is_valid():
+        authenticated_user = authenticate(
+            username = login_form.cleaned_data.get('username', ''),
+            password = login_form.cleaned_data.get('password', '')
+        )
+        
+        if authenticated_user is not None:
+            login(request, authenticated_user)
+
+            next_url = request.session.pop('next')
+            return redirect(next_url)
+        
+        messages.error(request, 'Usuário ou senha inválidos.')
+    else:
+        messages.error(request, 'Por favor, preencha os dois campos.')
+    
+    return redirect(login_url)
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('accounts:login'))
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('dashboard:index'))
+
+    signup_data = request.session.get('signup_data', None)
+    signup_form = SignupForm(signup_data)
+
+    return render(request, 'accounts/pages/signup.html', context={
+        'form': signup_form,
+        'form_action': reverse('accounts:perform_signup'),
+        'has_password_fields': True
+    })
+
+def perform_signup(request):
+    signup_url = reverse('accounts:signup')
+
+    if not request.POST:
+        return redirect(signup_url)
+
+    signup_data = request.POST
+    request.session['signup_data'] = signup_data
+    
+    signup_form = SignupForm(signup_data)
+    
+    if signup_form.is_valid():
+        user = signup_form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        
+        message = 'Usuário criado com sucesso.'
+        messages.success(request, message)        
+      
+        del request.session['signup_data']
+
+        return redirect(reverse('accounts:login'))
+
+    message = 'Por favor, preencha todos os campos corretamente.'
+    messages.error(request, message)  
+    
+    return redirect(signup_url)
+
 def reset_password(request):
     reset_password_url = reverse('accounts:perform_reset_password')
 
@@ -77,91 +165,3 @@ def perform_new_password(request, uidb64, token):
     messages.error(request, 'As senhas não são iguais.')
         
     return redirect(new_password_url)
-
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect(reverse('dashboard:index'))
-
-    signup_data = request.session.get('signup_data', None)
-    signup_form = SignupForm(signup_data)
-
-    return render(request, 'accounts/pages/signup.html', context={
-        'form': signup_form,
-        'form_action': reverse('accounts:perform_signup'),
-        'has_password_fields': True
-    })
-
-def perform_signup(request):
-    signup_url = reverse('accounts:signup')
-
-    if not request.POST:
-        return redirect(signup_url)
-
-    signup_data = request.POST
-    request.session['signup_data'] = signup_data
-    
-    signup_form = SignupForm(signup_data)
-    
-    if signup_form.is_valid():
-        user = signup_form.save(commit=False)
-        user.set_password(user.password)
-        user.save()
-        
-        message = 'Usuário criado com sucesso.'
-        messages.success(request, message)        
-      
-        del request.session['signup_data']
-
-        return redirect(reverse('accounts:login'))
-
-    message = 'Por favor, preencha todos os campos corretamente.'
-    messages.error(request, message)  
-    
-    return redirect(signup_url)
-
-def login_view(request):
-    dashboard_url = reverse('dashboard:index')
-    
-    if request.user.is_authenticated:
-        return redirect(dashboard_url)
-    
-    next = request.GET.get('next', '')
-    request.session['next'] = next or dashboard_url
-    
-    login_form = LoginForm()
-        
-    return render(request, 'accounts/pages/login.html', context={
-        'form': login_form,
-        'form_action': reverse('accounts:perform_login'),
-        'has_password_fields': True
-    })
-
-def perform_login(request):
-    login_url = reverse('accounts:login')
-    
-    if not request.POST:
-        return redirect(login_url)
-
-    login_form = LoginForm(request.POST)
-    
-    if login_form.is_valid():
-        authenticated_user = authenticate(
-            username = login_form.cleaned_data.get('username', ''),
-            password = login_form.cleaned_data.get('password', '')
-        )
-        
-        if authenticated_user is not None:
-            login(request, authenticated_user)
-
-            next_url = request.session.pop('next')
-            return redirect(next_url)
-        
-        messages.error(request, 'Usuário ou senha inválidos.')
-    else:
-        messages.error(request, 'Por favor, preencha os dois campos.')
-    
-    return redirect(login_url)
-
-def logout_view(request):
-    logout(request)
-    return redirect(reverse('accounts:login'))
