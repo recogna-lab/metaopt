43e6e27c29ae022a950f9d5bde3660a08d75d639
Giovani Candido <giovcandido@outlook.com>
2022-12-09 11:50:28 -0300

    Update dashboard login_required rule

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+redirect_to = 'accounts:login'
+next_redirect_field = 'next'
 
-@login_required(login_url='accounts:login', redirect_field_name='next')
+@login_required(redirect_to, next_redirect_field)
 def index(request):
     return render(request, 'dashboard/pages/index.html')
