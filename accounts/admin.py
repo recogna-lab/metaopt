8041c3d389570bb15397dd761ba6d0a32dfad7a3
Giovani Candido <giovcandido@outlook.com>
2022-12-09 20:01:33 -0300

    Customize user admin page so that id is visible

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'id', 'username', 'email', 'first_name', 'last_name', 'is_staff'
+    )
+    readonly_fields = ('id',)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
