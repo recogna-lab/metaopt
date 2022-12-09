ccd642cfd6a27d2a6d06c19ca4129428118e918a
Giovani Candido <giovcandido@outlook.com>
2022-12-09 20:00:58 -0300

    Register user task model in admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserTask
+
+
+class UserTaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task')
+    readonly_fields = ('user', 'task')
+
+admin.site.register(UserTask, UserTaskAdmin)
