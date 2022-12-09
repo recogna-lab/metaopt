430972635c7010b4afd9e63982335d314a2c5f00
Giovani Candido <giovcandido@outlook.com>
2022-12-09 16:14:22 -0300

    Add celery parameters to databases.py

--- metaopt/settings/databases.py
+++ metaopt/settings/databases.py
@@ -14,3 +14,11 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'django-db'
