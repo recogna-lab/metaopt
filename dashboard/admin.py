5715df790aeec0c84d5cb80f74b1844a20d844dd
Giovani Candido <giovcandido@outlook.com>
2022-12-13 11:37:43 -0300

    Improve user task admin page

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django_celery_results.models import TaskResult
 
 from .models import UserTask
 
 
 class UserTaskAdmin(admin.ModelAdmin):
-    list_display = ('user', 'task')
+    list_display = (
+        'user', 
+        'email', 
+        'first_name', 
+        'last_name', 
+        'task', 
+        'task_name', 
+        'date_created', 
+        'date_completed'
+    )
     readonly_fields = ('user', 'task')
-
+    
+    def get_user_object(self, obj):
+        user_object = User.objects.get(id=obj.user.id)
+        return user_object
+    
+    def email(self, obj):
+        return self.get_user_object(obj).email
+    
+    def first_name(self, obj):
+        return self.get_user_object(obj).first_name
+    
+    def last_name(self, obj):
+        return self.get_user_object(obj).last_name
+    
+    def get_task_object(self, obj):
+        task_object = TaskResult.objects.get(task_id=obj.task.task_id)
+        return task_object
+    
+    def task_id(self, obj):
+        return self.get_task_object(obj).task_id
+    
+    def task_name(self, obj):
+        return self.get_task_object(obj).task_name
+    
+    def task_status(self, obj):
+        return self.get_task_object(obj).status
+    
+    def date_created(self, obj):
+        return self.get_task_object(obj).date_created
+    
+    def date_completed(self, obj):
+        return self.get_task_object(obj).date_done
+    
 admin.site.register(UserTask, UserTaskAdmin)
