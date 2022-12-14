dc36082118651c865048e62a96feb97a089bf0dc
Giovani Candido <giovcandido@outlook.com>
2022-12-13 21:31:37 -0300

    Remove feature_selection task for now

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -8,11 +8,3 @@ def optimization(self, user_id, message):
     return {
         'output':  output,
     }
-
-# @app.task(name='feature_selection', bind=True)
-# def feature_selection(self, user_id, message):
-#     output = f'Feature Selection task says: {message}!'
-    
-#     return {
-#         'output':  output,
-#     }
