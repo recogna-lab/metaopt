43b1483308d548f058b743b213511dc8f2aa177c
Giovani Candido <giovcandido@outlook.com>
2022-12-13 21:20:34 -0300

    Remove dummy task from index view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .tasks import dummy_task
-
 
 @login_required(login_url='accounts:login', redirect_field_name='next')
 def index(request):
-    task = dummy_task.delay(user_id=request.user.id, message='And I say Hi!')
     return render(request, 'dashboard/pages/index.html')
