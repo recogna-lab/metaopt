09765ec801804dc06625e5217dd1684e622e6ea8
Giovani Candido <giovcandido@outlook.com>
2022-12-14 23:14:31 -0300

    Create urls/views for optimization tasks

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,19 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(
+        'opt/new/', 
+         views.new_optimization_task, 
+         name='new_optimization_task'
+    ),
+    path(
+        'opt/new/start/', 
+        views.start_optimization_task, 
+        name='start_optimization_task'
+    ),
+    path(
+        'opt/<task_id>/', 
+        views.optimization_task, 
+        name='optimization_task'
+    )
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 @login_required(login_url='accounts:login', redirect_field_name='next')
 def index(request):
     return render(request, 'dashboard/pages/index.html')
+
+@login_required(login_url='accounts:login', redirect_field_name='next')
+def new_optimization_task(request):
+    return HttpResponse('New Optimization Task')
+
+@login_required(login_url='accounts:login', redirect_field_name='next')
+def start_optimization_task(request):
+    return HttpResponse('Start Optimization Task')
+
+@login_required(login_url='accounts:login', redirect_field_name='next')
+def optimization_task(request, task_id):
+    return HttpResponse('Optimization Task')
