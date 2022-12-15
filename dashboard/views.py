2a0fb525eec06d5b3aefdb960834f7f1461784af
Giovani Candido <giovcandido@outlook.com>
2022-12-15 10:28:29 -0300

    Remove redirect_field_name since 'next' is default

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,18 +3,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-@login_required(login_url='accounts:login', redirect_field_name='next')
+@login_required(login_url='accounts:login')
 def index(request):
     return render(request, 'dashboard/pages/index.html')
 
-@login_required(login_url='accounts:login', redirect_field_name='next')
+@login_required(login_url='accounts:login')
 def new_optimization_task(request):
     return HttpResponse('New Optimization Task')
 
-@login_required(login_url='accounts:login', redirect_field_name='next')
+@login_required(login_url='accounts:login')
 def start_optimization_task(request):
     return HttpResponse('Start Optimization Task')
 
-@login_required(login_url='accounts:login', redirect_field_name='next')
+@login_required(login_url='accounts:login')
 def optimization_task(request, task_id):
     return HttpResponse('Optimization Task')
