b6ab49e8d62bd5bfed381fe594821d4f5976ebf3
Giovani Candido <giovcandido@outlook.com>
2022-12-15 10:33:30 -0300

    Remove login_url argument

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,18 +3,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-@login_required(login_url='accounts:login')
+@login_required
 def index(request):
     return render(request, 'dashboard/pages/index.html')
 
-@login_required(login_url='accounts:login')
+@login_required
 def new_optimization_task(request):
     return HttpResponse('New Optimization Task')
 
-@login_required(login_url='accounts:login')
+@login_required
 def start_optimization_task(request):
     return HttpResponse('Start Optimization Task')
 
-@login_required(login_url='accounts:login')
+@login_required
 def optimization_task(request, task_id):
     return HttpResponse('Optimization Task')
