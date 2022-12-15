6b7f0593538f30e54984845474d9349630f70c6b
Giovani Candido <giovcandido@outlook.com>
2022-12-15 10:40:55 -0300

    Shorten url names

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     path(
         'opt/new/', 
          views.new_optimization_task, 
-         name='new_optimization_task'
+         name='new_opt_task'
     ),
     path(
         'opt/new/start/', 
         views.start_optimization_task, 
-        name='start_optimization_task'
+        name='start_opt_task'
     ),
     path(
         'opt/<task_id>/', 
         views.optimization_task, 
-        name='optimization_task'
+        name='opt_task'
     )
 ]
