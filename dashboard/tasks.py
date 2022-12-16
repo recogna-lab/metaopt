1bc486761936a5332340272aefdb0c0188c630ca
Giovani Candido <giovcandido@outlook.com>
2022-12-16 18:03:22 -0300

    Store queued/pending tasks in the db

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -24,3 +24,25 @@ def optimization(self, user_id, optimizer, function, agents, iterations):
     return {
         'output':  output,
     }
+
+
+@signals.before_task_publish.connect
+def create_pending_task(headers=None, body=None, **kwargs):
+    # Check if there is a task obj in headers
+    if 'task' not in headers:
+        return
+    
+    hostname = headers['origin'].split('@')[1]
+    
+    # Create task with status=PENDING
+    TaskResult.objects.store_result(
+        content_encoding = 'utf-8',
+        content_type = 'application/json',
+        task_id=headers['id'],
+        status = states.PENDING,
+        result = None,
+        task_name = headers['task'],
+        task_args = json.dumps(headers['argsrepr']),
+        task_kwargs = json.dumps(headers['kwargsrepr']),
+        worker = f'celery@{hostname}'
+    )
