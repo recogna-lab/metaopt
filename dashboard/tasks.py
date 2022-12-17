94907ec39d17b0a35d9aa5ae64e192c6236abc6d
Giovani Candido <giovcandido@outlook.com>
2022-12-16 22:57:54 -0300

    Update loop total and add description to recorder

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -12,10 +12,15 @@ from metaopt.celery import app
 def optimization(self, user_id, optimizer, function, agents, iterations):
     progress_recorder = ProgressRecorder(self)
     
-    # Loop up to 10 sleeping 1 s in each iteration (simulated execution)
-    for i in range(20):
+    # Loop up to 50 sleeping 1 s in each iteration (simulated execution)
+    for i in range(50):
         time.sleep(1)
-        progress_recorder.set_progress(i + 1, 20)
+        
+        progress_recorder.set_progress(
+            current=i + 1, 
+            total=50, 
+            description='Tarefa em execução...'
+        )
         
     # Save the output
     output = f'I ran {optimizer} for {iterations} iterations!'
