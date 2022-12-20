6b8ab050b4235dcaee21e0016279aeea2db8a1a2
Giovani Candido <giovcandido@outlook.com>
2022-12-20 14:59:44 -0300

    Create callback to record progress

--- dashboard/tasks/optimization_task.py
+++ dashboard/tasks/optimization_task.py
@@ -1,8 +1,10 @@
 import numpy as np
+from celery_progress.backend import ProgressRecorder
 from opytimizer import Opytimizer
 from opytimizer.core import Function
 from opytimizer.optimizers.swarm import PSO
 from opytimizer.spaces import SearchSpace
+from opytimizer.utils.callback import Callback
 
 from metaopt.celery import app
 
@@ -10,43 +12,74 @@ from metaopt.celery import app
 class _OptimizationTask(app.Task):
     abstract = True
     
-    def optimize(self, _optimizer, _function, _agents, _iterations):        
+    def optimize(self, _optimizer, _function, _agents, _iterations):
+        # Set optimizer    
         optimizer = PSO()
         
+        # Set cost function
         function = lambda x: np.sum(x ** 2)
         function = Function(function)
         
+        # Set number of agents and iterations
         agents = 10
         iterations = 10
         
+        # Create search space
         space = SearchSpace(
             n_agents=agents, 
             n_variables=2, 
             lower_bound=[-10, -10], 
             upper_bound=[10, 10]
         )
-
-        opt = Opytimizer(space, optimizer, function)
         
-        opt.start(n_iterations=iterations)
+        # Create Opytimizer instance
+        opytimizer = Opytimizer(space, optimizer, function)
         
-        # Get results
-        optimum_value =  opt.space.best_agent.position.flatten().tolist()
-        function_value = opt.space.best_agent.fit.item()
+        # Create progress callback
+        progress_callback = self.create_progress_callback(total=iterations)
+        
+        # Start optimization
+        opytimizer.start(n_iterations=iterations, callbacks=[progress_callback])
+        
+        # Get optimum value and function on optimum
+        optimum_value =  opytimizer.space.best_agent.position.flatten().tolist()
+        function_value = opytimizer.space.best_agent.fit.item()
 
-        # Get errors
-        _, error_values = opt.history.get_convergence('best_agent')
+        # Get errors (for convergence plot)
+        _, error_values = opytimizer.history.get_convergence('best_agent')
         
         error_values = error_values.tolist()
         
-        # Create output dict
-        output = {
+        # Add results and errors to the output        
+        return {
             'optimum_value': optimum_value,
             'function_value': function_value,
             'error_values': error_values
         }
-        
-        return output
+
+    def create_progress_callback(self, total):
+        # Create custom callback class for Opytimizer
+        class ProgressCallback(Callback):
+            
+            def __init__(self, task, total):
+                # Create progress recorder associated with the task
+                self.progress_recorder = ProgressRecorder(task)
+                
+                # Save total number of iterations
+                self.total = total
+                
+                super().__init__()
+            
+            # At the beginning of each iteration, record the progress
+            def on_iteration_begin(self, curr_iteration, _):
+                self.progress_recorder.set_progress(
+                    current=curr_iteration,
+                    total=self.total, 
+                    description='Tarefa em execução...'
+                )
+
+        # Return custom callback instance
+        return ProgressCallback(self, total)
 
 @app.task(name='optimization', base=_OptimizationTask, bind=True)
 def optimization(self, user_id, optimizer, function, agents, iterations):
