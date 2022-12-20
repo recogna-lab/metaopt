a0bba3d0d9f7d1cbb6f404950bc79ff0dbf54bc4
Giovani Candido <giovcandido@outlook.com>
2022-12-19 23:36:56 -0300

    Create file for plotting related functions

---
+++ utils/plots.py
@@ -0,0 +1,47 @@
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+
+def plot_convergence(error_values):
+    # Create a list with the iterations
+    iterations = list(range(1, len(error_values) + 1))
+    
+    # Set up the plot
+    fig = go.Figure()
+    scatter = go.Scatter(
+        x=iterations,
+        y=error_values,
+        mode='lines',
+        opacity=0.8,
+        marker_color='blue')
+    fig.add_trace(scatter)
+    fig.update_layout(
+        title='Gráfico de Convergência',
+        title_x=0.5,
+        template='simple_white',
+        xaxis=dict(
+            title='Iteração',
+            showgrid=False,
+            rangemode='tozero',
+        ),
+        yaxis=dict(
+            title='Erro',
+            showgrid=False,
+            tickformat='.2e',
+        ),
+        margin=dict(
+            l=0, 
+            r=0, 
+            t=30, 
+            b=30
+        ),
+    )
+    
+    # Generate a div element with the plot
+    plot_div = plot(fig, output_type='div', config={
+        'displayModeBar': True,
+        'displaylogo': False,
+        'modeBarButtonsToRemove': ['resetScale']
+    })
+    
+    return plot_div
