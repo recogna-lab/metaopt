54428eae1c0b9caf75939ac686d82c8698fe7306
Giovani Candido <giovcandido@outlook.com>
2022-12-20 23:21:22 -0300

    Update comments, spacing and variable names

--- utils/transfer_functions.py
+++ utils/transfer_functions.py
@@ -1,96 +1,76 @@
-import opytimizer.math.random as r
 import math as m
+
 import numpy as np
+import opytimizer.math.random as r
 
-#Sigmoid function and it's variations for feature selection
+# Sigmoid function and some of its variations used in feature selection
+# They are applied to 'sol', which is the solution outputted by an optimizer
 
-def s1(otimization_solution):
-    """ S1 Transfer Function: The original Sigmoid fuction
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# S1 Transfer Function: The original Sigmoid fuction
+def s1(sol):
     features = []
 
-    for i in range(otimization_solution.size):
+    for i in range(sol.size):
         rand = r.generate_uniform_random_number()
         
-        if rand < 1.0 / (1.0 + m.exp(-1 * otimization_solution[i])):
+        if rand < 1.0 / (1.0 + m.exp(-1 * sol[i])):
             features.append(1)
         else:
             features.append(0)
 
     return np.asarray(features).astype(bool)
 
-def s2(otimization_solution):
-    """ S2 Transfer Function 
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# S2 Transfer Function
+def s2(sol):
     features = []
 
-    for i in range(otimization_solution.size):
-
+    for i in range(sol.size):
         rand = r.generate_uniform_random_number()
         
-        if rand < (1.0 / (1.0 + m.exp(-2 * otimization_solution[i]))):
-            #If the feature is selected
+        if rand < (1.0 / (1.0 + m.exp(-2 * sol[i]))):
+            # If the feature is selected
             features.append(1)
         else:
             features.append(0)
 
     return np.asarray(features).astype(bool)
 
-def s3(otimization_solution):
-    """ S3 Transfer Function 
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# S3 Transfer Function
+def s3(sol):
     features = []
 
-    for i in range(otimization_solution.size):
+    for i in range(sol.size):
+        
         rand = r.generate_uniform_random_number()
-        if rand < 1.0 / (1.0 + m.exp(-1 * otimization_solution[i] / 2)):
+        
+        if rand < 1.0 / (1.0 + m.exp(-1 * sol[i] / 2)):
             features.append(1)
         else:
             features.append(0)
 
     return np.asarray(features).astype(bool)
 
-def s4(otimization_solution):
-    """ S4 Transfer Function 
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# S4 Transfer Function
+def s4(sol):
     features = []
 
-    for i in range(otimization_solution.size):
+    for i in range(sol.size):
         rand = r.generate_uniform_random_number()
-        if rand < 1.0 / (1.0 + m.exp(-1 * otimization_solution[i] / 3)):
+        
+        if rand < 1.0 / (1.0 + m.exp(-1 * sol[i] / 3)):
             features.append(1)
         else:
             features.append(0)
 
     return np.asarray(features).astype(bool)
 
-def v1(otimization_solution):
-    """ V1 Transfer Function
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-    
+# V1 Transfer Function
+def v1(sol):
     features = []
 
-    for i, aux in enumerate(otimization_solution):
+    for _, aux in enumerate(sol):
         rand = r.generate_uniform_random_number()
+        
         if rand < m.fabs(m.erf(m.sqrt(m.pi) / 2 * (-1 * aux))):
             features.append(1)
         else:
@@ -98,56 +78,42 @@ def v1(otimization_solution):
 
     return np.asarray(features).astype(bool)
 
-def v2(otimization_solution):
-    """ V2 Transfer Function
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# V2 Transfer Function
+def v2(sol):
     features = []
 
-    for i, aux in enumerate(otimization_solution):
+    for _, aux in enumerate(sol):
         rand = r.generate_uniform_random_number()
+        
         if rand < m.fabs(m.tanh(-1 * aux)):
             features.append(1)
         else:
             features.append(0)
-
     
     return np.asarray(features).astype(bool)
 
-def v3(otimization_solution):
-    """ V3 Transfer Function
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# V3 Transfer Function
+def v3(sol):
     features = []
 
-    for i in range(otimization_solution.size):
+    for i in range(sol.size):
         rand = r.generate_uniform_random_number()
-        if rand < m.fabs(-1 * otimization_solution[i] / m.sqrt(1 + (-1 * (otimization_solution[i] * otimization_solution[i])))):
+        
+        if rand < m.fabs(-1 * sol[i] / m.sqrt(1 + (-1 * (sol[i] * sol[i])))):
             features.append(1)
         else:
             features.append(0)
-
     
     return np.asarray(features).astype(bool)
 
-def v4(otimization_solution):
-    """ V4 Transfer Function
-
-    Args:
-        otimization_solution (np.array): The best agent's position. 
-    """
-
+# V4 Transfer Function
+def v4(sol):
     features = []
 
-    for i in range(otimization_solution.size):
+    for i in range(sol.size):
         rand = r.generate_uniform_random_number()
-        if rand < m.fabs(2 / m.pi * m.atan(m.pi / 2 * (-1 * otimization_solution[i]))):
+        
+        if rand < m.fabs(2 / m.pi * m.atan(m.pi / 2 * (-1 * sol[i]))):
             features.append(1)
         else:
             features.append(0)
