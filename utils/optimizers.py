c0bd6d465f5635790b636ea5e17f9b3a0ee20234
Giovani Candido <giovcandido@outlook.com>
2022-12-20 22:19:31 -0300

    Only keep categories that are prob being used

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -1,9 +1,5 @@
-from opytimizer.optimizers.boolean import *
 from opytimizer.optimizers.evolutionary import *
-from opytimizer.optimizers.misc import *
-from opytimizer.optimizers.population import *
 from opytimizer.optimizers.science import *
-from opytimizer.optimizers.social import *
 from opytimizer.optimizers.swarm import *
 
 
