8dd3e1c6fdf2c589657a222fa2cfb86512b6b0b9
Giovani Candido <giovcandido@outlook.com>
2022-12-21 12:43:17 -0300

    Change features field to integer

--- dashboard/models.py
+++ dashboard/models.py
@@ -47,7 +47,7 @@ class Function(models.Model):
         verbose_name='Latex Expression',
         help_text='Latex expression for the function'
     )
-
+        
     def __str__(self):
         return f'{self.name}'
     
@@ -68,10 +68,9 @@ class Dataset(models.Model):
         help_text='File name of the dataset'
     )
 
-    features = models.CharField(
-        max_length=3,
-        verbose_name='Amount of features',
-        help_text='Amount of features of the dataset'
+    features = models.IntegerField(
+        verbose_name='Features',
+        help_text='Number of features of the dataset'
     )
     
     def __str__(self):
@@ -113,6 +112,7 @@ class UserTask(models.Model):
         ordering = ('-task__date_created', )
 
 
+# Before saving a task result instance
 @receiver(pre_save, sender=TaskResult)
 def format_task_kwargs(sender, instance, **kwargs):
     # Retrieve task named arguments as dict 
@@ -123,6 +123,7 @@ def format_task_kwargs(sender, instance, **kwargs):
     # Correctly save named arguments as json 
     instance.task_kwargs = json.dumps(task_kwargs_dict)
 
+# After saving a task result instance
 @receiver(post_save, sender=TaskResult)
 def save_user_task(sender, instance, created, **kwargs):
     # Return if instance already existed before save
