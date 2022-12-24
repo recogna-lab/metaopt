709b2d883eaeec06ae63160a83d85f5d61c581a1
Giovani Candido <giovcandido@outlook.com>
2022-12-24 15:36:53 -0300

    Accept custom description in constructor

--- utils/tasks/callback.py
+++ utils/tasks/callback.py
@@ -5,19 +5,30 @@ from opytimizer.utils.callback import Callback
 # Create custom callback class for Opytimizer
 class ProgressCallback(Callback):
     
-    def __init__(self, task, total):
+    DEFAULT_DESCRIPTION = 'Tarefa em execução...'
+    
+    def __init__(self, task, total, description):
         # Create progress recorder associated with the task
         self.progress_recorder = ProgressRecorder(task)
+        
+        # Set the progress description
+        self._set_description(description)
         
         # Save total number of iterations
         self.total = total
         
         super().__init__()
     
+    def _set_description(self, description):
+        if description is not None:
+            self.description = description
+        else:
+            self.description = self.DEFAULT_DESCRIPTION
+    
     # At the beginning of each iteration, record the progress
     def on_iteration_begin(self, curr_iteration, _):
         self.progress_recorder.set_progress(
             current=curr_iteration,
             total=self.total, 
-            description='Tarefa em execução...'
+            description=self.description
         )
