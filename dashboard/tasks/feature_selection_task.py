de4289b2ce6185c0a8941dbd4fe084e1b10ee064
Giovani Candido <giovcandido@outlook.com>
2022-12-24 17:57:25 -0300

    Partially handle many executions (deal w/ results)

--- dashboard/tasks/feature_selection_task.py
+++ dashboard/tasks/feature_selection_task.py
@@ -16,10 +16,118 @@ from utils.tasks.optimization import get_optimizer
 from .optimization_task import _OptimizationTask
 
 
+# This is the base class for the feature selection task
 class _FeatureSelectionTask(_OptimizationTask):
     
     abstract = True
 
+    def run_feature_selection(self, optimizer, dataset, transfer_function,
+                              dimension, agents, iterations, executions):
+        # Probrably a good idea to create a results class for fs too
+        # It's good to accumulate/average the results
+        # results = Result()
+        
+        # Save the number of executions
+        self.executions = executions
+        
+        # Run the feature selection n times:
+        for curr_exec in range(self.executions):
+            # Set progress description
+            self.set_progress_description(curr_exec + 1)
+            
+            # Get execution data
+            execution_data = self.select_features(
+                optimizer, 
+                dataset, 
+                transfer_function, 
+                dimension, 
+                agents, 
+                iterations
+            )
+            
+            # An update could be called here
+            # args -> execution data elements
+            # results.update(args)
+        
+        # Return the results (when you have it)
+        # return results
+
+        # For now, return the execution data
+        # which has the results from the 
+        # last feature selection execution
+        return execution_data
+        
+    def select_features(self, optimizer, dataset, transfer_function, 
+                        dimension, agents, iterations):
+        # Split dataset
+        self.dataset_split(dataset)
+
+        # Get transfer function
+        self.transfer_function = get_transfer_function(transfer_function)
+
+        # Set lower and upper bound
+        lower_bound = [0] * dimension
+        upper_bound = [1] * dimension
+
+        # Set space dict
+        space = {
+            'dimension': dimension, 
+            'lower_bound': lower_bound, 
+            'upper_bound': upper_bound
+        }
+
+        # Run optimize method
+        result_opt = self.optimize(optimizer, None, space, agents, iterations)
+
+        opf = SupervisedOPF(
+            distance='log_squared_euclidean',
+            pre_computed_distance=None
+        )
+
+        result_fs = self.testing_task(opf)
+
+        metrics = self.get_metrics(result_fs['confusion_matrix']) 
+        
+        return self.concatenate_results(result_opt, result_fs, metrics)
+    
+    def dataset_split(self, dataset):
+        # Take the path of dataset
+        dir = os.path.join(
+            BASE_DIR, 'dashboard/static/dashboard/datasets', dataset
+        )
+        
+        # Loading a .txt file to a numpy array
+        txt = l.load_txt(dir)
+        
+        # Parsing a pre-loaded numpy array
+        X, Y = p.parse_loader(txt)
+
+        # Split data into training and test sets
+        self.X_train, self.X_test, self.Y_train, self.Y_test = sp.split(
+            X, Y, percentage=0.5, random_state=1
+        )
+
+        # Training set will be splited into training and validation sets
+        self.X_train, self.X_val, self.Y_train, self.Y_val = sp.split(
+            self.X_train, self.Y_train, percentage = 0.2, random_state=1
+        )
+    
+    def optimize(self, optimizer, function, space, agents, iterations):
+        # Run super class method with desired function
+        return super().optimize(
+            optimizer, self.supervised_opf, space, agents, iterations
+        )
+    
+    def setup(self, optimizer, function, space, agents):
+        # Get and set the optimizer object    
+        self.optimizer = get_optimizer(optimizer)
+        
+        # Set the cost function
+        self.function = Function(function)
+
+        # Configure the search space
+        self.setup_space(agents, space)
+    
     def supervised_opf(self, opytimizer):
         # Transform the continuous solution in boolean solution (feature array) by applying the transfer function
         features = self.transfer_function(opytimizer)
@@ -52,82 +160,7 @@ class _FeatureSelectionTask(_OptimizationTask):
             self.best_selected_features = features
 
         return error
-
-    def optimize(self, optimizer, function, space, agents, iterations):
-        # Run super class method with desired function
-        return super().optimize(
-            optimizer,
-            self.supervised_opf,
-            space,
-            agents,
-            iterations
-        )
     
-    def setup(self, optimizer, function, space, agents):
-        # Get and set the optimizer object    
-        self.optimizer = get_optimizer(optimizer)
-        
-        # Set the cost function
-        self.function = Function(function)
-
-        # Configure the search space
-        self.setup_space(agents, space)
-
-    def select_features(self, optimizer, dataset, transfer_function, 
-                        dimension, agents, iterations):
-        # Split dataset
-        self.dataset_split(dataset)
-
-        # Get transfer function
-        self.transfer_function = get_transfer_function(transfer_function)
-
-        # Set lower and upper bound
-        lower_bound = [0] * dimension
-        upper_bound = [1] * dimension
-
-        # Set space dict
-        space = {
-            'dimension': dimension, 
-            'lower_bound': lower_bound, 
-            'upper_bound': upper_bound
-        }
-
-        # Run optimize method
-        result_opt = self.optimize(optimizer, None, space, agents, iterations)
-
-        opf = SupervisedOPF(
-            distance='log_squared_euclidean',
-            pre_computed_distance=None
-        )
-
-        result_fs = self.testing_task(opf)
-
-        metrics = self.metrics(result_fs['confusion_matrix']) 
-        
-        return self.concatenate_results(result_opt, result_fs, metrics)
-
-    def dataset_split(self, dataset):
-        # Take the path of dataset
-        dir = os.path.join(
-            BASE_DIR, 'dashboard/static/dashboard/datasets', dataset
-        )
-        
-        # Loading a .txt file to a numpy array
-        txt = l.load_txt(dir)
-        
-        # Parsing a pre-loaded numpy array
-        X, Y = p.parse_loader(txt)
-
-        # Split data into training and test sets
-        self.X_train, self.X_test, self.Y_train, self.Y_test = sp.split(
-            X, Y, percentage=0.5, random_state=1
-        )
-
-        # Training set will be splited into training and validation sets
-        self.X_train, self.X_val, self.Y_train, self.Y_val = sp.split(
-            self.X_train, self.Y_train, percentage = 0.2, random_state=1
-        )
-
     def testing_task(self, opf):
         # Remake training and tests subgraphs with selected features
         X_train_selected = self.X_train[:, self.best_selected_features]
@@ -150,15 +183,7 @@ class _FeatureSelectionTask(_OptimizationTask):
             'confusion_matrix': confusion_matrix.tolist()
         }
 
-    def concatenate_results(self, *results):
-        result = {}
-
-        for r in results:
-            result.update(r)
-
-        return result
-    
-    def metrics(self, confusion_matrix):
+    def get_metrics(self, confusion_matrix):
         confusion_matrix = np.array(confusion_matrix)
         rows, _ = confusion_matrix.shape
 
@@ -183,16 +208,27 @@ class _FeatureSelectionTask(_OptimizationTask):
             'recall': recall,
             'f1_score': f1_score
         }
+    
+    def concatenate_results(self, *results):
+        result = {}
 
+        for r in results:
+            result.update(r)
+
+        return result
+
+
+# This is the feature selection task
 @app.task(name='feature_selection', base=_FeatureSelectionTask, bind=True)
 def feature_selection(self, user_id, optimizer, dataset, transfer_function,
                       dimension, agents, iterations, executions):
-    # Run base class method
-    return self.select_features(
+    # Run the feature selection task as specified by the following arguments
+    return self.run_feature_selection(
         optimizer, 
         dataset, 
         transfer_function, 
         dimension, 
         agents, 
-        iterations
+        iterations,
+        executions
     )
